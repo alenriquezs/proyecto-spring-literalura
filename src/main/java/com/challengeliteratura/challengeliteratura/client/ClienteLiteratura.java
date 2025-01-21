9ee30b8c4d25f1fc00bffb552fc36ef6daecfb60
Alfonso Enriquez <aenrique@espol.edu.ec>
2025-01-21 14:44:27 -0600

    Carga del proyecto con sus funcionalidades e implementaciones

---
+++ src/main/java/com/challengeliteratura/challengeliteratura/client/ClienteLiteratura.java
@@ -0,0 +1,200 @@
+package com.challengeliteratura.challengeliteratura.client;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.challengeliteratura.challengeliteratura.entity.AutorEntity;
+import com.challengeliteratura.challengeliteratura.entity.LibroEntity;
+import com.challengeliteratura.challengeliteratura.mapper.ConvierteDatos;
+import com.challengeliteratura.challengeliteratura.model.Respuesta;
+import com.challengeliteratura.challengeliteratura.repository.AutorRepository;
+import com.challengeliteratura.challengeliteratura.repository.LibroRepository;
+import com.challengeliteratura.challengeliteratura.service.ConsumoAPI;
+
+public class ClienteLiteratura {
+
+    private final String URL_BASE = "https://gutendex.com/books/?search=";
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    private LibroRepository libroRepositorio;
+    private AutorRepository autorRepositorio;
+
+    public ClienteLiteratura(LibroRepository libroRepositorio, AutorRepository autorRepositorio) {
+        this.libroRepositorio = libroRepositorio;
+        this.autorRepositorio = autorRepositorio;
+    }
+
+    public void menu() {
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = """
+					Escoja la opción que desea realizar:
+						1.- Buscar libro por titulo
+						2.- Lista de libros registrados
+						3.- Lista de autores registrados
+						4.- Lista de autores vivos en un determinado año
+						5.- Lista de libros por idioma
+						0 - Salir
+						""";
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    buscarLibroWeb();
+                    break;
+                case 2:
+                    buscarLibros();
+                    break;
+                case 3:
+                    buscarAutores();
+                    break;
+                case 4:
+                    buscarAutoresVivo();
+                    break;
+                case 5:
+                    buscarPorIdiomas();
+                    break;
+                case 0:
+                    System.out.println("Cerrando aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+
+    private void buscarLibros() {
+
+        List<LibroEntity> libros = libroRepositorio.findAll();
+
+        if (!libros.isEmpty()) {
+
+            for (LibroEntity libro : libros) {
+                System.out.println("\n\n---------- LIBROS -------\n");
+                System.out.println(" Titulo: " + libro.getTitulo());
+                System.out.println(" Autor: " + libro.getAutor().getNombre());
+                System.out.println(" Idioma: " + libro.getLenguaje());
+                System.out.println(" Descargas: " + libro.getDescargas());
+                System.out.println("\n-------------------------\n\n");
+            }
+
+        } else {
+            System.out.println("\n\n ***** NO SE ENCONTRARON RESULTADOS ***** \n\n");
+        }
+
+    }
+
+    private void buscarAutores() {
+        List<AutorEntity> autores = autorRepositorio.findAll();
+
+        if (!autores.isEmpty()) {
+            for (AutorEntity autor : autores) {
+                System.out.println("\n\n---------- Autores -------\n");
+                System.out.println(" Nombre: " + autor.getNombre());
+                System.out.println(" Fecha de Nacimiento: " + autor.getFechaNacimiento());
+                System.out.println(" Fecha de Fallecimiento: " + autor.getFechaFallecimiento());
+                System.out.println(" Libros: " + autor.getLibros().getTitulo());
+                System.out.println("\n-------------------------\n\n");
+            }
+        } else {
+            System.out.println("\n\n ***** NO SE ENCONTRARON RESULTADOS ***** \n\n");
+
+        }
+
+    }
+
+    private void buscarAutoresVivo() {
+        System.out.println("Escriba el año de búsqueda: ");
+        var anio = teclado.nextInt();
+        teclado.nextLine();
+
+        List<AutorEntity> autores = autorRepositorio.findForYear(anio);
+
+        if (!autores.isEmpty()) {
+            for (AutorEntity autor : autores) {
+                System.out.println("\n\n---------- Autores Vivos -------\n");
+                System.out.println(" Nombre: " + autor.getNombre());
+                System.out.println(" Fecha de nacimiento: " + autor.getFechaNacimiento());
+                System.out.println(" Fecha de fallecimiento: " + autor.getFechaFallecimiento());
+                System.out.println(" Libros: " + autor.getLibros().getTitulo());
+                System.out.println("\n-------------------------\n\n");
+            }
+        } else {
+            System.out.println("\n\n ***** NO SE ENCONTRARON RESULTADOS ***** \n\n");
+
+        }
+    }
+
+    private void buscarPorIdiomas() {
+
+        var menu = """
+				Seleccione un Idioma:
+					1.- Español
+					2.- Inglés
+				
+					""";
+        System.out.println(menu);
+        var idioma = teclado.nextInt();
+        teclado.nextLine();
+
+        String seleccion = "";
+
+        if(idioma == 1) {
+            seleccion = "es";
+        } else 	if(idioma == 2) {
+            seleccion = "en";
+        }
+
+        List<LibroEntity> libros = libroRepositorio.findForLanguaje(seleccion);
+
+        if (!libros.isEmpty()) {
+
+            for (LibroEntity libro : libros) {
+                System.out.println("\n\n---------- LIBROS POR IDIOMA-------\n");
+                System.out.println(" Titulo: " + libro.getTitulo());
+                System.out.println(" Autor: " + libro.getAutor().getNombre());
+                System.out.println(" Idioma: " + libro.getLenguaje());
+                System.out.println(" Descargas: " + libro.getDescargas());
+                System.out.println("\n-------------------------\n\n");
+            }
+
+        } else {
+            System.out.println("\n\n ***** NO SE ENCONTRARON RESULTADOS ***** \n\n");
+        }
+
+
+    }
+
+    private void buscarLibroWeb() {
+        Respuesta datos = getDatosSerie();
+
+        if (!datos.results().isEmpty()) {
+
+            LibroEntity libro = new LibroEntity(datos.results().get(0));
+            libro = libroRepositorio.save(libro);
+
+        }
+
+        System.out.println("Datos: ");
+        System.out.println(datos);
+    }
+
+    private Respuesta getDatosSerie() {
+        System.out.println("Ingrese el nombre del libro que desea buscar: ");
+        var titulo = teclado.nextLine();
+        titulo = titulo.replace(" ", "%20");
+        System.out.println("Titulo : " + titulo);
+        System.out.println(URL_BASE + titulo);
+        var json = consumoApi.obtenerDatos(URL_BASE + titulo);
+        System.out.println(json);
+        Respuesta datos = conversor.obtenerDatos(json, Respuesta.class);
+        return datos;
+    }
+
+}
+
